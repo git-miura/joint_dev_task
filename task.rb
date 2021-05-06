67ea3df01957bf6b196e355034b340ba964e5adb
三浦 <miura.mai910@gmail.com>
2021-05-06 12:48:54 +0900

    q4,8の修正　q9~16まで回答

--- task.rb
+++ task.rb
@@ -98,8 +98,7 @@ def q11
 
   # 以下に回答を記載
   puts "ユーザーの趣味一覧"
-  sports.flatten!.uniq!
-  sports.each.with_index(1) do |sport , i|
+  sports.flatten!.uniq!.each.with_index(1) do |sport , i|
     puts "No#{i} #{sport}"
   end
 end
